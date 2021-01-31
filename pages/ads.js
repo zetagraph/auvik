273e96ee6640ac368c0c2c1c70229ab2067dacb6
Andrei Zvonkov <andrei@appnovation.com>
2021-01-31 18:03:50 -0500

    add updates

--- pages/ads.js
+++ pages/ads.js
@@ -1,36 +1,24 @@
 import Head from 'next/head'
-import Header from '../components/header'
-import Footer from '../components/footer'
 
 export default function Ads() {
-  const Images = styled.div`
-    padding: 8rem 4rem;
-    text-align: center;
-
-    @media (min-width: ${(props) => props.theme.screen.lg}) {
-      padding: 14rem 4rem;
-    }
-
-    img {
-      display: inline-block;
-      margin-bottom: 2rem;
-    }
-  `
   return (
     <>
       <Head>
         <title>Auvik ebook</title>
         <meta name="description" content="auvik" />
       </Head>
-      <Header />
+
       <main>
-        <Images>
+        <div>
           <img src="/images/ad-600x600.png" alt="ad" />
+        </div>
+        <div>
           <img src="/images/ad-600x600.png" alt="ad" />
+        </div>
+        <div>
           <img src="/images/ad-728x90.png" alt="ad" />
-        </Images>
+        </div>
       </main>
-      <Footer />
     </>
   )
 }
