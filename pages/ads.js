43876bc1fd807821197ec2137d0d13a456dcab0c
Andrei Zvonkov <andrei@appnovation.com>
2021-01-31 17:56:50 -0500

    ads update

--- pages/ads.js
+++ pages/ads.js
@@ -3,6 +3,19 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 
 export default function Ads() {
+  const Images = styled.div`
+    padding: 8rem 4rem;
+    text-align: center;
+
+    @media (min-width: ${(props) => props.theme.screen.lg}) {
+      padding: 14rem 4rem;
+    }
+
+    img {
+      display: inline-block;
+      margin-bottom: 2rem;
+    }
+  `
   return (
     <>
       <Head>
@@ -11,7 +24,11 @@ export default function Ads() {
       </Head>
       <Header />
       <main>
-        Ads
+        <Images>
+          <img src="/images/ad-600x600.png" alt="ad" />
+          <img src="/images/ad-600x600.png" alt="ad" />
+          <img src="/images/ad-728x90.png" alt="ad" />
+        </Images>
       </main>
       <Footer />
     </>
